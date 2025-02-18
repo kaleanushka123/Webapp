2b111778e254c562339bbef38577f0c2f1f09071
kaleanushka123 <130952266+kaleanushka123@users.noreply.github.com>
2025-02-18 16:02:20 +0530

    flask-webapp

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-import os
-from flask import Flask
+ from flask import Flask
+
 app = Flask(__name__)
 
-@app.route("/")
-def main():
-    return "Welcome!"
+@app.route('/')
+def home():
+    return "<h1>Welcome to My Simple Web App</h1><p>This is the home page.</p>"
 
-@app.route('/how are you')
-def hello():
-    return 'I am good, how about you?'
+@app.route('/about')
+def about():
+    return "<h1>About</h1><p>This is the about page.</p>"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+if __name__ == '__main__':
+    app.run(debug=True)
+
